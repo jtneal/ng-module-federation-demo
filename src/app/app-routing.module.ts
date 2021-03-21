70dc54170b032faf85d12f7cfc7bc184a1c87f0c
Jason Neal <jasonneal1@quickenloans.com>
2021-03-21 09:25:30 -0400

    ng add ng-module-federation --project shell --type shell --port 4200

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { createMicroFrontendDynamicRoute } from 'ng-module-federation';
+import { microFrontends } from '../micro-frontends';
 
-const routes: Routes = [];
+const routes: Routes = [...Object.keys(microFrontends).map((m) => createMicroFrontendDynamicRoute(microFrontends[m])),];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
