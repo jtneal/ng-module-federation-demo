a3ecd7609bad515b786b252c9ecb77dce58dbed1
Jason Neal <jasonneal1@quickenloans.com>
2021-03-21 09:25:09 -0400

    ng add ng-module-federation --project mfe2 --type micro --port 4220

--- projects/mfe2/src/app/app.module.ts
+++ projects/mfe2/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Mfe2Module } from './mfe2/mfe2.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Mfe2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
